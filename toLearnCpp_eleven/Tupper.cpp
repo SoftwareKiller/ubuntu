4115cbd59f939c9c9967bb64c78d397972717137
aliUbuntu <378961928@qq.com>
2019-09-28 23:11:00 +0800

    一个简单的Tuple模拟实现

--- toLearnCpp_eleven/Tupper.cpp
+++ toLearnCpp_eleven/Tupper.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+#if 0
 template<int IDX, int MAX, typename...Args>
 struct PRINT_TUPLE{
     static void Print(ostream& os, const tuple<Args...>& t)
@@ -31,5 +32,39 @@ ostream& operator<<(ostream& os, const tuple<Args...>& t)
 int main()
 {
     cout << make_tuple(7.5, string("hello"), bitset<16>(377), 42);
+}
+#endif
+
+template<typename...Values>
+class Tuple;
+template<> class Tuple<>{};
+
+template<typename Head, typename... Tail>
+class Tuple<Head, Tail...> : private Tuple<Tail...>
+{
+    typedef Tuple<Tail...> inherited;
+public:
+    Tuple(){}
+    Tuple(Head v, Tail... vtail)
+        :m_head(v), inherited(vtail...) //这里的inherited循环创建initialization list,调用父类的构造
+    {}
+    Head head()
+    {
+        return m_head;
+    }
+    inherited& tail()
+    {
+        return *this;
+    }
+protected:
+    Head m_head;
+};
+
+int main()
+{
+    Tuple<int, double, string> tp(8, 9.6, "Template is cool");
+    cout << tp.head() << endl;
+    cout << tp.tail().head() << endl;
+    cout << tp.tail().tail().head() << endl;
 }
 
