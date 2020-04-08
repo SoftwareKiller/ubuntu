d0a7aabfdfbb21a8db04b9f3df5bdbcdab1f081c
Jayden <378961928@qq.com>
2020-04-08 10:08:36 +0800

    一个异步日志的实现，性能大约在每秒10W条

---
+++ AsyncLog/main.cpp
@@ -0,0 +1,25 @@
+#include <chrono>
+#include <iostream>
+#include "src/AsyncLog.h"
+
+using namespace std;
+
+int main()
+{
+	CAsyncLog::init("./test");
+	chrono::time_point<chrono::system_clock, chrono::microseconds> now = chrono::time_point_cast<chrono::microseconds>(
+		chrono::system_clock::now());
+    unsigned long long start = now.time_since_epoch().count();
+	for (int i = 0; i < 10000; ++i)
+	{
+		LOGI("Today is April 7, num i is %d", i);
+	}
+
+	now = chrono::time_point_cast<chrono::microseconds>(
+		chrono::system_clock::now());
+    unsigned long long end = now.time_since_epoch().count();
+
+    LOGI("TIME:%llu", end - start);
+    CAsyncLog::uninit();
+}
+
