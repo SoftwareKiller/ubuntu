aa51dbac0709b1dcf52f7aa79e4773454c23eb87
Jayden <378961928@qq.com>
2020-06-29 17:05:21 +0800

    romanToInt

--- algorithm/intToRoman.cpp
+++ algorithm/intToRoman.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
@@ -98,12 +99,36 @@ string intToRoman(int num)
     return s;
 }
 
+//罗马数字转整数
+int romanToInt(string s)
+{
+    unordered_map<char, int> dict = { {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000} };
+    int ret = 0;
+    int len = s.size() - 1;
+    for(int i = 0; i < len; ++i)
+    {
+        //利用了罗马数字逢4开头的就将下一进位的字母填充到当前位之后的特性
+        //如 4，9
+        //   40,90
+        //   ...
+        //   400, 900
+        dict[s[i]] < dict[s[i+1]] ? ret -= dict[s[i]] : ret += dict[s[i]];
+    }
+    ret += dict[s.back()];
+    return ret;
+}
+
 int main()
 {
-    cout << intToRoman(3) << endl;
-    cout << intToRoman(4) << endl;
-    cout << intToRoman(9) << endl;
-    cout << intToRoman(58) << endl;
-    cout << intToRoman(1994) << endl;
+    cout << intToRoman(3) << " ";
+    cout << romanToInt(intToRoman(3)) << endl;
+    cout << intToRoman(4) << " ";
+    cout << romanToInt(intToRoman(4)) << endl;
+    cout << intToRoman(9) << " ";
+    cout << romanToInt(intToRoman(9)) << endl;
+    cout << intToRoman(58) << " ";
+    cout << romanToInt(intToRoman(58)) << endl;
+    cout << intToRoman(1994) << " ";
+    cout << romanToInt(intToRoman(1994)) << endl;
 }
 
