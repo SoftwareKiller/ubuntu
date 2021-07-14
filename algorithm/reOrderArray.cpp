b4482a00eaa4aac63343f9cbd8c7f22e7f07c131
jayden <378961928@qq.com>
2021-07-15 00:15:13 +0800

    奇数位于偶数之前，且相对位置不变

--- algorithm/reOrderArray.cpp
+++ algorithm/reOrderArray.cpp
@@ -33,6 +33,30 @@ vector<int> reOrderArray(vector<int>& array)
     return result;
 }
 
+vector<int> bubbleSort(vector<int>& array)
+{
+    if(array.size() <= 1)
+    {
+        return array;
+    }
+
+    bool flag = true;
+    while(flag)
+    {
+        flag = false;
+        for(size_t j = 0; j < array.size(); ++j)
+        {
+            if(j > 0 && (array.at(j - 1) & 0x1) == 0 && (array.at(j) & 0x1))
+            {
+                swap(array[j-1], array[j]);
+                flag = true;
+            }
+        }
+    }
+
+    return array;
+}
+
 void print(vector<int>& v)
 {
     for(auto& it : v)
@@ -46,7 +70,8 @@ void print(vector<int>& v)
 int main()
 {
     vector<int> v = {2,4,6,5,7};
-    auto res = reOrderArray(v);
+    //auto res = reOrderArray(v);
+    auto res = bubbleSort(v);
     print(res);
     return 0;
 }
