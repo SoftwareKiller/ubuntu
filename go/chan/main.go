9d18a72f6b94157d5522bcf3a4d4688020065c97
jayden <378961928@qq.com>
2022-01-04 23:53:29 +0800

    add codes

--- go/chan/main.go
+++ go/chan/main.go
@@ -1,10 +1,22 @@
 package main
 
 import (
-	"chan/msgqueue"
+	"bytes"
+	"chan/gotask"
+	"chan/insettype"
+	"fmt"
+	"io"
+	"os"
+	"runtime"
 )
 
 func main() {
-	// tennis.Game()
-	msgqueue.Start()
+	// game.Game()
+	// msgqueue.Start()
+	var b bytes.Buffer
+	b.Write([]byte("Hello"))
+	fmt.Fprintf(&b, " World! Mac CPU:%v\n", runtime.NumCPU())
+	io.Copy(os.Stdout, &b)
+	insettype.InsetTest()
+	gotask.GoTask()
 }
